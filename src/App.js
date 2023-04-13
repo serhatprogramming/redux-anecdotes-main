e27c4516dec00a4884a3c5a5e3b2d973ccfc61be
serhatprogramming <serhatprogramming@gmail.com>
2023-04-13 17:55:25 -0400

    add anectode reducer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
+import { createAnectode } from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   const vote = (id) => {
-    console.log('vote', id)
-  }
+    console.log("vote", id);
+  };
+
+  const addAnectode = (event) => {
+    event.preventDefault();
+    const content = event.target.anectode.value;
+    console.log("content: ", content);
+    event.target.anectode.value = "";
+    dispatch(createAnectode(content));
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnectode}>
+        <div>
+          <input name="anectode" />
+        </div>
+        <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
