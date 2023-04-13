97780e7a301e66ccc57f105bcd4c702fc79f7099
serhatprogramming <serhatprogramming@gmail.com>
2023-04-13 18:06:21 -0400

    sorting by the votes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ const App = () => {
   const anecdotes = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const vote = (id) => {
-    console.log("vote", id);
-  };
-
   const addAnectode = (event) => {
     event.preventDefault();
     const content = event.target.anectode.value;
@@ -20,17 +16,19 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(increaseVote(anecdote.id))}>
-              vote
-            </button>
+      {anecdotes
+        .sort((a, b) => b.votes - a.votes)
+        .map((anecdote) => (
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={() => dispatch(increaseVote(anecdote.id))}>
+                vote
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
       <h2>create new</h2>
       <form onSubmit={addAnectode}>
         <div>
