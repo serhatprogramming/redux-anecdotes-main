b23833c4971deb0c3224c3fd56f3ddf9192e7d0c
serhatprogramming <serhatprogramming@gmail.com>
2023-04-14 19:13:48 -0400

    json-server integrated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useEffect } from "react";
+
 import AnectodeForm from "./components/AnectodeForm";
 import AnectodeList from "./components/AnectodeList";
 import Filter from "./components/Filter";
 
+import anecdoteService from "./services/anectodes";
+
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+import { useDispatch } from "react-redux";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService.getAll().then((data) => dispatch(setAnecdotes(data)));
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
